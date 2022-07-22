a6d64374a67e49910749371bd0ca084168d018ff
Radek Milner <rmilner@prograils.com>
2022-07-22 11:18:59 +0200

    prepare data about athletes their countries sport

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+ISO3166::Country.all.shuffle.first(30).each do |country|
+  Country.create({name: country.name})
+end
+
+100.times { Sporter.create(
+  {
+    name: Faker::Name.name,
+    age: rand(18..40),
+    country_id: rand(1..30)
+  }
+) }
+
+individual_sports = %w[tennis parachuting badminton archery chess boxing racing golf running skiing walking cycling surfing swimming]
+
+individual_sports.each do |c|
+  Competition.create( {title: c} )
+end
+
+Competition.all.each do |c|
+  sporters = Sporter.all.shuffle.first(10)
+  (1..10).each do |spot|
+    CompetitionResult.create({ 
+      sporter_id: sporters.pop.id
+      competition_id: competition.id
+      place: place,
+      created_at: rand(1.years.ago..Time.now)
+    })
+  end
+end

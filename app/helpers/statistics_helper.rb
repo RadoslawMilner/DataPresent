d8b6a0d2b6276e5b08c165b2be0b9ae36bfd7305
Radek Milner <rmilner@prograils.com>
2022-07-24 10:47:59 +0200

    notes & change path thanks to collection

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,8 +1,13 @@
 module StatisticsHelper
   def sporters_by_age
-    bar_chart @sporters.group(:age).count, height: '500px', library: {
+    # use defined method bar_chart from chartkick
+    # draw chart
+    # draw its height and by using hashes write how this chart have to look like
+    # sporters by age path comes from collection routes
+    bar_chart sporters_by_age_charts_path, height: '500px', library: {
       title: {text: 'Sporters by age', x: -20},
       yAxis: {
+        # we can't afford to have any decimals numbers in age
         allowDecimals: false,
         title: {
           text: 'Ages count'

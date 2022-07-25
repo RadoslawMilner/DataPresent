4e41ce47cef8d4b59529fb89dff078c22a68979b
Radek Milner <rmilner@prograils.com>
2022-07-25 18:01:01 +0200

    massive correction of data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,22 @@ ISO3166::Country.all.shuffle.first(30).each do |country|
   Country.create({name: country })
 end
 
-100.times { Sporter.create(
-  {
+100.times { Sporter.create({
     name: Faker::Name.name,
     age: rand(18..40),
     country_id: rand(1..30)
-  }
-) }
+  }) }
 
-individual_sports = %w[tennis parachuting badminton archery chess boxing racing golf running skiing walking cycling surfing swimming]
-
-individual_sports.each do |c|
+%w[tennis parachuting badminton archery chess boxing racing golf running skiing walking cycling surfing swimming].each do |c|
   Competition.create( {title: c} )
 end
 
 Competition.all.each do |c|
-  sporters = Sporter.all.shuffle.first(10)
+  @sporters = Sporter.all.shuffle.first(10)
   (1..10).each do |spot|
     CompetitionResult.create({ 
-      sporter_id: sporters.pop.id,
-      competition_id: c.id,
+      sporter_id: @sporters.pop,
+      competition_id: c,
       place: spot,
       created_at: rand(1.years.ago..Time.now)
     })
